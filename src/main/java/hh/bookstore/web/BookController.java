f16cffe254618ca773b8734c71c11145d543e152
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2024-09-16 19:18:17 +0300

    addbook toiminto lisätty

--- src/main/java/hh/bookstore/web/BookController.java
+++ src/main/java/hh/bookstore/web/BookController.java
@@ -5,13 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import hh.bookstore.domain.Book;
 import hh.bookstore.domain.BookRepository;
 
 @Controller
-
 public class BookController {
 
     @Autowired
@@ -27,19 +29,28 @@ public class BookController {
     @GetMapping(value = "/booklist")
     public String getBookList(Model model) {
         model.addAttribute("books", repository.findAll());
-        return "booklist"; //booklist.html
+        return "booklist"; // booklist.html
     }
 
     // http://localhost:8080/addbook
     @GetMapping(value = "/addbook")
     public String addBook(Model model) {
         model.addAttribute("book", new Book());
-        return "addbook"; //addbook.html
+        return "addbook"; // addbook.html
     }
-    
+
+    // tallentaa lomakkeen tiedot
     @PostMapping("/save")
     public String saveBook(@ModelAttribute Book book) {
+        repository.save(book);
         return "redirect:/booklist";
+    }
+
+    // poistaa id:llä kirjan
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long studentId, Model model) {
+        repository.deleteById(studentId);
+        return "redirect:../studentlist";
     }
 
 }
