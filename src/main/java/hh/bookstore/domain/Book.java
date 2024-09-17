1625e96a70642f3502643b91879d292b80ad8409
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2024-09-17 09:25:10 +0300

    edit toimii. entityyn lisätty id:lle setteri.

--- src/main/java/hh/bookstore/domain/Book.java
+++ src/main/java/hh/bookstore/domain/Book.java
@@ -70,14 +70,19 @@ public class Book {
         this.price = price;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     @Override
     public String toString() {
         return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
                 + ", price=" + price + "]";
     }
+
 
 }
