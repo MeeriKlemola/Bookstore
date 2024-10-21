c4b662fe833a23282a192079d273ac209a1c6a5e
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2024-10-21 20:57:09 +0300

    dockerfile lisätty

--- src/test/java/hh/bookstore/BookstoreApplicationTests.java
+++ src/test/java/hh/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,20 @@
 package hh.bookstore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hh.bookstore.web.BookController;
 
 @SpringBootTest
 class BookstoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private BookController controller;
 
+		@Test
+		public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+		}
 }
